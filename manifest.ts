f6e0883e775e0a56e0d9c922029bad1e6b7bc002
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:59:35 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,12 +2,9 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Sudoku',
-  icon: 'uil:table',
   routes: {
     '/': lazy(() => import('@')),
     '/play/:sessionId': lazy(() => import('@/pages/Play')),
     '/print/:sessionId': lazy(() => import('@/pages/Print'))
   },
-  category: 'Utilities'
 } satisfies ModuleConfig
